e31f23fa0f1d1f7c39b85780473a8c89dd44f9f2
Alex Chen <alexschen94@gmail.com>
2015-08-06 21:54:30 -0700

    Add edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,31 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
   def new
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
+    # @user = User.new(username: params[:username], password: params[:password], email: params[:email])
     @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
     else
       render :new
+    end
+  end
+
+  def update
+    @user = User.new
+    if @user.update(user_params)
+      redirect_to edit_user_paths
+    else 
+      render :edit
     end
   end
 
